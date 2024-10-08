7a6a3fdac0eee937c1104a02707698c5dba4d3ee
SergeyAlexandrovich <85061748+podlesniy@users.noreply.github.com>
2024-10-08 10:22:12 +0300

    DatabaseMappers - replaced with a normal mapper

--- data/src/main/java/ua/czrblz/data/mappers/DatabaseMappers.kt
+++ data/src/main/java/ua/czrblz/data/mappers/DatabaseMappers.kt
@@ -5,37 +5,25 @@ import ua.czrblz.domain.model.VideoModel
 
 class DatabaseMappers {
 
-    fun toVideoModel(list: List<VideoDBModel>): List<VideoModel> {
-        val arrayList: ArrayList<VideoModel> = ArrayList()
-        list.forEach {
-            arrayList.add(
-                VideoModel(
-                    id = it.id,
-                    title = it.title,
-                    duration = it.duration,
-                    videoUrl = it.videoUrl,
-                    videoThumbnail = it.videoThumbnail,
-                    likes = it.likes
-                )
-            )
-        }
-        return arrayList
+    fun toVideoModel(list: List<VideoDBModel>): List<VideoModel> = list.map {
+        VideoModel(
+            id = it.id,
+            title = it.title,
+            duration = it.duration,
+            videoUrl = it.videoUrl,
+            videoThumbnail = it.videoThumbnail,
+            likes = it.likes
+        )
     }
 
-    fun toVideoDBModel(list: List<VideoModel>): List<VideoDBModel> {
-        val arrayList: ArrayList<VideoDBModel> = ArrayList()
-        list.forEach {
-            arrayList.add(
-                VideoDBModel(
-                    id = it.id,
-                    title = it.title,
-                    duration = it.duration,
-                    videoUrl = it.videoUrl,
-                    videoThumbnail = it.videoThumbnail,
-                    likes = it.likes
-                )
-            )
-        }
-        return arrayList
+    fun toVideoDBModel(list: List<VideoModel>): List<VideoDBModel> = list.map {
+        VideoDBModel(
+            id = it.id,
+            title = it.title,
+            duration = it.duration,
+            videoUrl = it.videoUrl,
+            videoThumbnail = it.videoThumbnail,
+            likes = it.likes
+        )
     }
 }
