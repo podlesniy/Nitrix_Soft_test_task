7d765b542308aee373e633ae5ed1cc358e5aefe1
SergeyAlexandrovich <85061748+podlesniy@users.noreply.github.com>
2024-10-08 10:20:55 +0300

    VideoMappers - replaced with a normal mapper

--- data/src/main/java/ua/czrblz/data/mappers/VideoMappers.kt
+++ data/src/main/java/ua/czrblz/data/mappers/VideoMappers.kt
@@ -5,20 +5,14 @@ import ua.czrblz.domain.model.VideoModel
 
 class VideoMappers {
 
-    fun toVideoModel(list: List<Video>): List<VideoModel> {
-        val arrayList: ArrayList<VideoModel> = ArrayList()
-        list.forEach {
-            arrayList.add(
-                VideoModel(
-                    id = it.id,
-                    title = it.tags,
-                    duration = it.duration,
-                    videoUrl = it.videos.tiny.url,
-                    videoThumbnail = it.videos.tiny.thumbnail,
-                    likes = it.likes
-                )
-            )
-        }
-        return arrayList
+    fun toVideoModel(list: List<Video>): List<VideoModel> = list.map {
+        VideoModel(
+            id = it.id,
+            title = it.tags,
+            duration = it.duration,
+            videoUrl = it.videos.tiny.url,
+            videoThumbnail = it.videos.tiny.thumbnail,
+            likes = it.likes
+        )
     }
 }
